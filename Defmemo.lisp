f933ee46a03d3fd703413029a5a3e238433413b4
Clarissa Littler <clarissa.littler@gmail.com>
2015-10-12 09:51:25 -0700

    tiny comment fix

--- Defmemo.lisp
+++ Defmemo.lisp
@@ -1,7 +1,7 @@
 ;;;; oh, huh, it's possible to make a macro that allows you to define automatically memoized functions, isn't it?
 
 
-;; this isn't pretty but it does, in fact, work
+;; this isn't pretty but it does, in fact, work 
 (defmacro defmemo (name args &body body)
   (let* ((g (gensym)))
     `(let ((,g (make-hash-table)))
